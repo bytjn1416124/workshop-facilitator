eade2406003cfde918aa2fd99f7a08102192d508
bytjn1416124 <65572341+bytjn1416124@users.noreply.github.com>
2025-01-02 17:04:36 +0000

    Update widgets exports

---
+++ client/src/components/ui/widgets/index.ts
@@ -0,0 +1,5 @@
+export { ChecklistWidget } from './ChecklistWidget';
+export { FlowchartWidget } from './FlowchartWidget';
+export { DataTableWidget } from './DataTableWidget';
+export { ChartWidget } from './ChartWidget';
+export { WidgetSelector } from './WidgetSelector';
