18a5058e35e3f589ea162daedda359e89da90fef
bytjn1416124 <65572341+bytjn1416124@users.noreply.github.com>
2025-01-02 17:51:30 +0000

    Update Next.js configuration

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   output: 'standalone',
-  experimental: {}
+  experimental: {},
+  env: {
+    NEXT_PUBLIC_WS_URL: 'ws://localhost:8000',
+    NEXT_PUBLIC_API_URL: 'http://localhost:8000'
+  }
 }
 
 module.exports = nextConfig
